f1005bb4876a043070092a9b9bfd945d4187d548
Zach Carrera <zachcarrera@gmail.com>
2023-04-08 07:58:49 -0700

    add test directory

---
+++ src/test/java/com/zachcarrera/hellohuman/HelloHumanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachcarrera.hellohuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
